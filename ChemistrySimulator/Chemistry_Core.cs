a008956678d9808ceb2423fc23e22e39ab9201ca
unknown <qoo0302@naver.com>
2018-11-27 21:18:30 +0900

    Created core files and ready to implement details

---
+++ ChemistrySimulator/Chemistry_Core.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChemistrySimulator
+{
+    public class Chemistry_Core
+    {
+
+    }
+
+    class ChemNotation
+    {
+        public const int ion_H = 0;
+        public const int ion_Cl = 1;
+        public const int ion_K = 2;
+        public const int ion_Na = 3;
+        public const int ion_OH = 4;
+        public const int H2O = 5;
+    }
+
+    class Beaker
+    {
+        // H+, Cl-, K+, Na+, OH-, H2O, respectively.
+        // All components will be initialized with default value 0
+        private float[] beakerComponents = new float[6] { 0, 0, 0, 0, 0, 0 };
+
+        public float getTotalBeakerVolume()
+        {
+            return beakerComponents[ChemNotation.ion_Cl]
+                + beakerComponents[ChemNotation.ion_Na]
+                + beakerComponents[ChemNotation.ion_K];
+        } 
+
+        public float getComponentVolume(int component) {
+            return beakerComponents[component];
+        }
+
+        // You'll know why i did this when you learn about neutralization reaction...
+        public void addBeakerComponent(int component, float amount)
+        {
+            if (amount > 0)
+            {
+                switch (component)
+                {
+                    case ChemNotation.ion_Cl:
+                        beakerComponents[ChemNotation.ion_H] += amount;
+                        beakerComponents[ChemNotation.ion_Cl] += amount;
+                        break;
+
+                    case ChemNotation.ion_K:
+                        beakerComponents[ChemNotation.ion_K] += amount;
+                        beakerComponents[ChemNotation.ion_OH] += amount;
+                        break;
+
+                    case ChemNotation.ion_Na:
+                        beakerComponents[ChemNotation.ion_Na] += amount;
+                        beakerComponents[ChemNotation.ion_OH] += amount;
+                        break;
+
+                    case ChemNotation.H2O:
+                        beakerComponents[ChemNotation.H2O] += amount;
+                        break;
+                }
+            }
+        }
+
+        public void removeBeakerComponent(int component, float amount)
+        {
+            if(getComponentVolume(component) >= amount)
+            {
+                switch (component)
+                {
+                    case ChemNotation.ion_Cl:
+                        beakerComponents[ChemNotation.ion_H] -=
+                            Math.Min(beakerComponents[ChemNotation.ion_Cl], beakerComponents[ChemNotation.ion_H]);
+                        beakerComponents[ChemNotation.ion_Cl] -=
+                            Math.Min(beakerComponents[ChemNotation.ion_Cl], beakerComponents[ChemNotation.ion_H]);
+                        break;
+
+                    case ChemNotation.ion_K:
+                        beakerComponents[ChemNotation.ion_K] -=
+                            Math.Min(beakerComponents[ChemNotation.ion_K], beakerComponents[ChemNotation.ion_OH]);
+                        beakerComponents[ChemNotation.ion_OH] -=
+                            Math.Min(beakerComponents[ChemNotation.ion_K], beakerComponents[ChemNotation.ion_OH]);
+                        break;
+
+                    case ChemNotation.ion_Na:
+                        beakerComponents[ChemNotation.ion_Na] -=
+                            Math.Min(beakerComponents[ChemNotation.ion_Na], beakerComponents[ChemNotation.ion_OH]);
+                        beakerComponents[ChemNotation.ion_OH] -=
+                            Math.Min(beakerComponents[ChemNotation.ion_Na], beakerComponents[ChemNotation.ion_OH]);
+                        break;
+                }
+            }
+        }
+    }
+
+    class Reaction
+    {
+        public void neutralize(Beaker beaker)
+        {
+            float H = beaker.getComponentVolume(ChemNotation.ion_H);
+            float OH = beaker.getComponentVolume(ChemNotation.ion_OH);
+
+            if(H > 0 && OH > 0)
+            {
+                float Min = Math.Min(H, OH);
+                beaker.removeBeakerComponent(ChemNotation.ion_H, Min);
+                beaker.removeBeakerComponent(ChemNotation.ion_OH, Min);
+                beaker.addBeakerComponent(ChemNotation.H2O, Min);
+            }
+        }
+    }
+
+}
---
+++ ChemistrySimulator/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChemistrySimulator
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        // For testing purpose. Using default instance instead
+        Beaker defaultBeaker = new Beaker();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void renewBeakerStatus()
+        {
+            BeakerStatus.Content = defaultBeaker.getTotalBeakerVolume().ToString() + " mL";
+        }
+
+        // For testing purpose, 10 mL will be added. must take user's input through any possible methods
+
+        void Beaker_MouseEnterEvent(object sender, MouseEventArgs e)
+        {
+            BeakerStatus.Content = "";
+            for(int i=0;i<6;i++)
+            {
+                BeakerStatus.Content += defaultBeaker.getComponentVolume(i).ToString() + " ";
+            }
+        }
+
+        void Beaker_MouseLeaveEvent(object sender, MouseEventArgs e)
+        {
+            renewBeakerStatus();
+        }
+
+        void HClBeaker_LeftMouseDownEvent(object sender, MouseButtonEventArgs e)
+        {
+            defaultBeaker.addBeakerComponent(ChemNotation.ion_H, 10);
+            defaultBeaker.addBeakerComponent(ChemNotation.ion_Cl, 10);
+            renewBeakerStatus();
+        }
+
+        void NaOHBeaker_LeftMouseDownEvent(object sender, MouseButtonEventArgs e)
+        {
+            defaultBeaker.addBeakerComponent(ChemNotation.ion_Na, 10);
+            defaultBeaker.addBeakerComponent(ChemNotation.ion_OH, 10);
+            renewBeakerStatus();
+        }
+
+        void KOHBeaker_LeftMouseDownEvent(object sender, MouseButtonEventArgs e )
+        {
+            defaultBeaker.addBeakerComponent(ChemNotation.ion_K, 10);
+            defaultBeaker.addBeakerComponent(ChemNotation.ion_OH, 10);
+            renewBeakerStatus();
+        }
+    }
+}
