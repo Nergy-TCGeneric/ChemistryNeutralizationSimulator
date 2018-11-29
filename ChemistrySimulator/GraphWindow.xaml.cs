2ec7817c6105a8fd9c24bd32d8c14bfb628a0a63
TCGeneric <qoo0302@naver.com>
2018-11-29 17:41:19 +0900

    Added third-party library(LiveCharts). Ready to implement graph.

--- ChemistrySimulator/GraphWindow.xaml.cs
+++ ChemistrySimulator/GraphWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace ChemistrySimulator
 {
@@ -24,4 +26,9 @@ namespace ChemistrySimulator
             InitializeComponent();
         }
     }
+}
+
+namespace Wpf.CartesianChart.PointShapeLine
+{
+    
 }
